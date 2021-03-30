8cef397e530a891c9cad9f7c76d7c6d2bdbc5d8d
efryters <efryters@gmail.com>
2021-03-29 23:54:35 -0400

    Finish keymap- need to test

--- keyboards/mykeeb/keymaps/default/keymap.c
+++ keyboards/mykeeb/keymaps/default/keymap.c
@@ -30,13 +30,23 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_A,    KC_1,    MO(_FN),
-            KC_TAB,   KC_SPC
+        KC_ESCAPE,  KC_TAB,         KC_DELETE,      KC_FN0,     \
+        KC_NUMLOCK, KC_KP_SLASH,    KC_KP_ASTERISK, KC_KP_MINUS,\
+        KC_KP_7,    KC_KP_8,        KC_KP_9,        KC_KP_PLUS, \
+        KC_KP_4,    KC_KP_5,        KC_KP_6,                    \
+        KC_KP_1,    KC_KP_2,        KC_KP_3,                    \
+        KC_KP_0,    KC_KP_DOT,      KC_KP_ENTER                  \
     ),
-    [_FN] = LAYOUT(
-        QMKBEST, QMKURL,  _______,
-            RESET,    XXXXXXX
-    )
+
+    
+    // [_FN] = LAYOUT(
+    //     KC_ESCAPE,  KC_TAB,         KC_DELETE,      KC_FN0,
+    //     KC_NUMLOCK, KC_KP_SLASH,    KC_KP_ASTERISK, KC_KP_MINUS,
+    //     KC_KP_7,    KC_KP_8,        KC_KP_9,        KC_KP_PLUS,
+    //     KC_KP_4,    KC_KP_5,        KC_KP_6,        
+    //     KC_KP_1,    KC_KP_2,        KC_KP_3,        
+    //     KC_KP_0,    KC_KP_DOT,      KC_KP_ENTER,    
+    // ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {

318fa6a21fe6ae7a5ed6c869157b86457efcf609
efryters <efryters@gmail.com>
2021-04-17 00:18:45 -0400

    Add oled support and added layer 2

--- keyboards/mykeeb/keymaps/v0.1-layout/keymap.c
+++ keyboards/mykeeb/keymaps/v0.1-layout/keymap.c
@@ -30,7 +30,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_ESCAPE,  KC_TAB,         KC_DELETE,      KC_FN0,     \
+        KC_ESCAPE,  KC_TAB,         KC_DELETE,      MO(1),     \
         KC_NUMLOCK, KC_KP_SLASH,    KC_KP_ASTERISK, KC_KP_MINUS,\
         KC_KP_7,    KC_KP_8,        KC_KP_9,        KC_KP_PLUS, \
         KC_KP_4,    KC_KP_1,        KC_KP_5,                  \
@@ -39,14 +39,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     
-    // [_FN] = LAYOUT(
-    //     KC_ESCAPE,  KC_TAB,         KC_DELETE,      KC_FN0,
-    //     KC_NUMLOCK, KC_KP_SLASH,    KC_KP_ASTERISK, KC_KP_MINUS,
-    //     KC_KP_7,    KC_KP_8,        KC_KP_9,        KC_KP_PLUS,
-    //     KC_KP_4,    KC_KP_5,        KC_KP_6,        
-    //     KC_KP_1,    KC_KP_2,        KC_KP_3,        
-    //     KC_KP_0,    KC_KP_DOT,      KC_KP_ENTER,    
-    // ),
+    /* FN */
+    [_FN] = LAYOUT(
+        KC_MEDIA_PREV_TRACK,  KC_MEDIA_PLAY_PAUSE,         KC_MEDIA_NEXT_TRACK,      KC_TRNS,     \
+        KC_CALC, KC_BSPACE,    KC_AUDIO_MUTE, KC_AUDIO_VOL_DOWN,\
+        KC_KP_7,    KC_KP_8,        KC_KP_9,        KC_AUDIO_VOL_UP, \
+        KC_KP_4,    KC_KP_1,        KC_KP_5,                  \
+        KC_KP_2,    KC_KP_6,        KC_KP_3,                  \
+        KC_KP_0,    KC_KP_DOT,      KC_KP_ENTER                  \
+    ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -70,3 +71,50 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
+
+#ifdef OLED_DRIVER_ENABLE
+void oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case _BASE:
+            oled_write_P(PSTR("Default\n"), false);
+            break;
+        case _FN:
+            oled_write_P(PSTR("FN\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    oled_write_P(PSTR("\n"), false);
+    switch (get_highest_layer(layer_state)) {
+        case _BASE:
+            oled_write_P(PSTR("ESC  TAB  DEL  FN\n"), false);
+            break;
+        case _FN:
+            oled_write_P(PSTR("|<   ||   >|     \n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+}
+
+// static void render_logo(void) {
+//     static const char PROGMEM qmk_logo[] = {
+//         0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+//         0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+//         0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+//     };
+
+//     oled_write_P(qmk_logo, false);
+// }
+#endif
